3a6f605ee4fc58be993c6c83debd18e5fc2e5253
Nikolay Liemiekhov <nicklemster@gmail.com>
2020-09-08 14:39:02 +0300

    Добавляет переменную видимости колонки

--- source/js/component/color-component.js
+++ source/js/component/color-component.js
@@ -1,29 +1,34 @@
 import AbstractComponent from "./abstract-component";
+import {columnVisibility} from "../utils/variables";
 
 const createColorTemplate = (colors) => colors.map((it) => {
   return (
     `<tr>
-      <td class="table__id">${it.id}</td>
-      <td class="table__name">${it.name}</td>
-      <td class="table__year">${it.year}</td>
-      <td class="table__color">${it.color}</td>
-      <td class="table__pantone">${it.pantoneValue}</td>
+      ${columnVisibility.ID ? `<td class="table__id">${it.id}</td>` : ``}
+      ${columnVisibility.NAME ? `<td class="table__name">${it.name}</td>` : ``}
+      ${columnVisibility.YEAR ? `<td class="table__year">${it.year}</td>` : ``}
+      ${columnVisibility.COLOR ? `<td class="table__color">${it.color}</td>` : ``}
+      ${columnVisibility.PANTONE ? `<td class="table__pantone">${it.pantoneValue}</td>` : ``}
     </tr>`
   );
 }).join(``);
 
 const createColorsTemplate = (colors) => {
-  console.log(colors)
   return (
-    `<table class="table">
-      <tr>
-        <th class="table__id">ID</th>
-        <th class="table__name">Name</th>
-        <th class="table__year">Year</th>
-        <th class="table__color">Color</th>
-        <th class="table__pantone">Pantone value</th>
-      </tr>
-      ${createColorTemplate(colors)}`
+    `<section class="color-section">
+      <h1>Pantone colors</h1>
+      <button class="color-section__btn-reset" type="button">Reset</button>
+      <table class="table">
+        <tr>
+          ${columnVisibility.ID ? `<th class="table__id">ID</th>` : ``}
+          ${columnVisibility.NAME ? `<th class="table__name">Name</th>` : ``}
+          ${columnVisibility.YEAR ? `<th class="table__year">Year</th>` : ``}
+          ${columnVisibility.COLOR ? `<th class="table__color">Color</th>` : ``}
+          ${columnVisibility.PANTONE ? `<th class="table__pantone">Pantone value</th>` : ``}
+        </tr>
+        ${createColorTemplate(colors)}
+      </table>
+    </section>`
   );
 };
 
---
+++ source/js/utils/variables.js
@@ -0,0 +1,9 @@
+const columnVisibility = {
+  ID: true,
+  NAME: true,
+  YEAR: false,
+  COLOR: true,
+  PANTONE: true,
+};
+
+export {columnVisibility};
